726a59e17668498966b426ee3233f38cebc2b5ca
Roger Bassons Renart <rogerbassons@gmail.com>
2018-03-21 10:50:14 -0400

    simplified xdg script

--- xdgDirs.sh
+++ xdgDirs.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-dataMountPoint='/mnt/dades'
-dataDevice='/dev/sdaX'
+dataMountPoint='/mnt/data'
 
-set=0
-sudo mkdir $dataMountPoint && sudo echo "$dataDevice $dataMountPoint ntfs-3g rw,uid=1000,gid=1000,dmask=0002,fmask=0003 0 0" >> /etc/fstab && set=1
+dirs=(Documents Downloads Music Pictures Videos)
+for i in "${dirs[@]}"
+do
+    #echo $i
+    rm -r $i
+    ln -s $dataMountPoint/$i 
+done
 
-if [ "$set" -eq "0" ]; then
-
-	xdg-user-dirs-update
-        
-        dirs=(Documents Downloads Music Pictures Videos)
-	for i in "$dirs[@]}"
-	do
-		mv ~/$i ~/$ibackup
-	        ln -s $dataMountPoint/$i ~/$i
-		cp -r ~/$ibackup/* ~/$i/
-	done
-fi
 
 
 
